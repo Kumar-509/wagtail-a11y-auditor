cfb5ac028af82dd53d2ae817396e437469d0639e
Kumar-509 <kumarcse42@gmail.com>
2025-03-29 10:38:05 +0530

    Initial project structure for GSoC 2025 proposal

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wagtail Accessibility Auditor 
+Accessibility auditing tool for Wagtail CMS 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="wagtail-a11y-auditor",
+    version="0.1.0",
+    description="Accessibility auditing tool for Wagtail CMS",
+    author="KUMAR",
+    author_email="kumarcse42@gmail.com",
+    packages="wagtail_a11y_auditor",
+    install_requires="wagtail>=6.0",
+)
